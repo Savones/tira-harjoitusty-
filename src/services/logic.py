5366e0cbebe6b9e2b9abb8a163fcef113c97a0fc
Essi Savonius <essi.savonius@hotmail.fi>
2024-03-17 19:46:55 +0200

    Generate random rooms by button press

--- src/services/logic.py
+++ src/services/logic.py
@@ -1,2 +1,10 @@
+from random import randrange
+
+
 class Logic():
-    pass
+    def generate_rooms(self, amount):
+        rooms = []
+        for i in range(amount):
+            rooms.append((randrange(0, 800), randrange(0, 800),
+                         randrange(3, 75), randrange(3, 75)))
+        return rooms
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -4,12 +4,12 @@ import pygame
 class UI():
     def __init__(self, logic) -> None:
         self.logic = logic
+        self.screen = pygame.display.set_mode([800, 900])
 
     def start(self):
         pygame.init()
-
-        screen = pygame.display.set_mode([800, 900])
         pygame.display.set_caption('Luolaston generointi työkalu')
+        self.screen.fill((255, 255, 255))
 
         running = True
         while running:
@@ -17,19 +17,28 @@ class UI():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.Rect(300, 825, 200, 50).collidepoint(event.pos):
+                        rooms = self.logic.generate_rooms(20)
+                        for room in rooms:
+                            self.create_rect((198, 235, 190), room)
 
-            self.draw_backgroud(screen)
+            self.draw_backgroud()
 
             pygame.display.flip()
 
         pygame.quit()
 
-    def draw_backgroud(self, screen):
-        screen.fill((255, 255, 255))
-        pygame.draw.rect(screen, (219, 50, 77), [0, 800, 800, 100])
-        font = pygame.font.Font('freesansbold.ttf', 24)
-        text = font.render('Generoi luolasto', True,
-                           (0, 0, 0), (255, 255, 255))
-        textRect = text.get_rect()
-        textRect.center = (400, 850)
-        screen.blit(text, textRect)
+    def draw_backgroud(self):
+        self.create_rect((219, 50, 77), [0, 800, 800, 100])
+        self.create_rect((0, 0, 10), [300, 825, 200, 50])
+        self.create_text('Generoi luolasto', (255, 255, 255),
+                         (312, 838), pygame.font.Font('freesansbold.ttf', 22))
+
+    def create_rect(self, color, position):
+        pygame.draw.rect(self.screen, color, position)
+
+    def create_text(self, text, color, position, font):
+        text_rect = font.render(text, True,
+                                color)
+        self.screen.blit(text_rect, position)
