e940a2350965a5050761672cc9a11bc304c8b92b
Essi Savonius <essi.savonius@hotmail.fi>
2024-03-19 16:50:50 +0200

    Test RoomService class

---
+++ src/tests/services/test_room_service.py
@@ -0,0 +1,39 @@
+import unittest
+from services.room_service import RoomService
+from entities.room import Room
+
+
+class TestRoomService(unittest.TestCase):
+    def setUp(self) -> None:
+        self.room_service = RoomService()
+        self.checked_room = Room(100, 100, 100, 100)
+
+    def test_check_overlap_return_true_when_no_x_overlap(self):
+        rooms = [Room(400, 100, 100, 100)]
+        self.assertEqual(
+            True, self.room_service.check_overlap(self.checked_room, rooms))
+
+    def test_check_overlap_return_true_when_no_y_overlap(self):
+        rooms = [Room(100, 400, 100, 100)]
+        self.assertEqual(
+            True, self.room_service.check_overlap(self.checked_room, rooms))
+
+    def test_check_overlap_return_false_when_overlap(self):
+        rooms = [Room(100, 100, 100, 100)]
+        self.assertEqual(
+            False, self.room_service.check_overlap(self.checked_room, rooms))
+
+    def test_check_measurements_return_false_when_x_unfit(self):
+        room = Room(700, 100, 100, 100)
+        self.assertEqual(
+            False, self.room_service.check_measurements(room))
+
+    def test_check_measurements_return_false_when_y_unfit(self):
+        room = Room(100, 700, 100, 100)
+        self.assertEqual(
+            False, self.room_service.check_measurements(room))
+
+    def test_check_measurements_return_true_when_all_fit(self):
+        room = Room(100, 100, 100, 100)
+        self.assertEqual(
+            True, self.room_service.check_measurements(room))
