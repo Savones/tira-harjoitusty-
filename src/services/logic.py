d412d60c2c51fbb7a71c47544acbda2d36e9050a
Essi Savonius <essi.savonius@hotmail.fi>
2024-03-19 15:45:41 +0200

    Check that rooms don't overlap

--- src/services/logic.py
+++ src/services/logic.py
@@ -8,6 +8,23 @@ class Logic:
 
     def generate_rooms(self, amount) -> list:
         for _ in range(amount):
-            self.rooms.append(Room(randrange(0, 800), randrange(0, 800),
-                                   randrange(3, 75), randrange(3, 75)))
+            while True:
+                room = Room(randrange(50, 740), randrange(50, 740),
+                            randrange(10, 150), randrange(10, 150))
+                if self.check_measurements(room) and self.check_overlap(room):
+                    self.rooms.append(room)
+                    break
         return self.rooms
+
+    def check_overlap(self, checked_room) -> bool:
+        for room in self.rooms:
+            if checked_room.x + checked_room.width > room.x - 50 and checked_room.x < room.x + room.width + 50:
+                if checked_room.y + checked_room.height > room.y - 50 and checked_room.y < room.y + room.height + 50:
+                    return False
+        return True
+
+    def check_measurements(self, room):
+        if room.x + room.width <= 750:
+            if room.y + room.height <= 750:
+                return True
+        return False
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,5 +1,10 @@
 import pygame
 
+GREEN = (198, 235, 190)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (219, 50, 77)
+
 
 class UI:
     def __init__(self, logic) -> None:
@@ -9,7 +14,7 @@ class UI:
     def start(self):
         pygame.init()
         pygame.display.set_caption('Luolaston generointi työkalu')
-        self.screen.fill((255, 255, 255))
+        self.screen.fill(WHITE)
 
         running = True
         while running:
@@ -19,21 +24,18 @@ class UI:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if pygame.Rect(300, 825, 200, 50).collidepoint(event.pos):
-                        rooms = self.logic.generate_rooms(20)
+                        rooms = self.logic.generate_rooms(10)
                         for room in rooms:
                             self.create_rect(
-                                (198, 235, 190), (room.x, room.y, room.width, room.height))
-
+                                GREEN, (room.x, room.y, room.width, room.height))
             self.draw_backgroud()
-
             pygame.display.flip()
-
         pygame.quit()
 
     def draw_backgroud(self):
-        self.create_rect((219, 50, 77), [0, 800, 800, 100])
-        self.create_rect((0, 0, 10), [300, 825, 200, 50])
-        self.create_text('Generoi luolasto', (255, 255, 255),
+        self.create_rect(RED, [0, 800, 800, 100])
+        self.create_rect(BLACK, [300, 825, 200, 50])
+        self.create_text('Generoi luolasto', WHITE,
                          (312, 838), pygame.font.Font('freesansbold.ttf', 22))
 
     def create_rect(self, color, position):
