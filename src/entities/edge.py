c88efbbfc77f44a1308e5a7f704772bdf8c4eed1
Essi Savonius <essi.savonius@hotmail.fi>
2024-03-30 23:17:48 +0200

    Simplify Edge class

--- src/entities/edge.py
+++ src/entities/edge.py
@@ -1,5 +1,3 @@
 class Edge:
     def __init__(self, vertex1: tuple, vertex2: tuple):
-        self.vertex1 = vertex1
-        self.vertex2 = vertex2
-        self.edge = [vertex1, vertex2]
+        self.edge = (vertex1, vertex2)
