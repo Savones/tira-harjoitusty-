6db898f8c4371750b4d593fdc1e30a6fe63301f6
Essi Savonius <essi.savonius@hotmail.fi>
2024-03-30 23:20:22 +0200

    Add methods that checks if room in supertriangle

--- src/services/room_service.py
+++ src/services/room_service.py
@@ -14,3 +14,25 @@ class RoomService:
             if room.y + room.height <= 750:
                 return True
         return False
+
+    def check_inside_triangle(self, room, triangle_vertices) -> bool:
+        room_corners = [(room.x, room.y), (room.x + room.width, room.y),
+                        (room.x, room.y + room.height), (room.x + room.width, room.y + room.height)]
+
+        for corner in room_corners:
+            if not self.check_point_inside_triangle(corner, triangle_vertices):
+                return False
+        return True
+
+    def check_point_inside_triangle(self, point, triangle):
+        def sign(p1, p2, p3):
+            return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+        b1 = sign(point, (triangle.vertex1.x, triangle.vertex1.y),
+                  (triangle.vertex2.x, triangle.vertex2.y)) < 0.0
+        b2 = sign(point, (triangle.vertex2.x, triangle.vertex2.y),
+                  (triangle.vertex3.x, triangle.vertex3.y)) < 0.0
+        b3 = sign(point, (triangle.vertex3.x, triangle.vertex3.y),
+                  (triangle.vertex1.x, triangle.vertex1.y)) < 0.0
+
+        return (b1 == b2) and (b2 == b3)
