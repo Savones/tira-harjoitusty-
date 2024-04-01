182ff2fadc2927d52d4162df4a2f240450d95940
Essi Savonius <essi.savonius@hotmail.fi>
2024-04-01 19:33:38 +0300

    Update ui

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -24,33 +24,44 @@ class UI:
         while running:
 
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                     running = False
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
+
                     if pygame.Rect(500, 925, 200, 50).collidepoint(event.pos):
-                        triangle_vertices = [[0, 898], [599, 0], [1198, 898]]
-                        pygame.draw.polygon(
-                            self.screen, RED, triangle_vertices, 1)
+                        self.logic.reset()
+                        self.screen.fill(WHITE)
+
                         rooms = self.logic.generate_rooms(4)
                         room_vertices = self.logic.generate_room_vertices()
+                        triangulation = self.logic.get_triangulation()
+
                         for room in rooms:
                             self.create_rect(
                                 GREEN, (room.x, room.y, room.width, room.height))
+
                         for vertex in room_vertices:
                             self.create_circle(BLACK, vertex, 5, 0)
-                        triangulation = self.logic.get_trianglutation()
+                            self.create_text(str(vertex), BLACK, vertex, pygame.font.Font(
+                                'freesansbold.ttf', 22))
+
                         for triangle in triangulation[0]:
                             vertices = [(triangle.vertex1.x, triangle.vertex1.y),
                                         (triangle.vertex2.x, triangle.vertex2.y),
                                         (triangle.vertex3.x, triangle.vertex3.y)]
                             pygame.draw.polygon(
                                 self.screen, RED, vertices, 1)
+
                         for triangle in triangulation[1]:
                             vertices = [(triangle.vertex1.x, triangle.vertex1.y),
                                         (triangle.vertex2.x, triangle.vertex2.y),
                                         (triangle.vertex3.x, triangle.vertex3.y)]
                             pygame.draw.polygon(
-                                self.screen, BLUE, vertices, 3)
+                                self.screen, BLUE, vertices, 2)
+                            pygame.draw.circle(
+                                self.screen, BLACK, triangle.circum_center, triangle.radius, 1, )
 
             self.draw_backgroud()
             pygame.display.flip()
