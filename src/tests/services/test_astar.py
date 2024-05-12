6b13c2c65052533cb6759f956c1b1b08c2dcb0d1
Essi Savonius <essi.savonius@hotmail.fi>
2024-05-12 21:34:52 +0300

    Add astar tests

--- src/tests/services/test_astar.py
+++ src/tests/services/test_astar.py
@@ -26,3 +26,47 @@ class TestAstar(unittest.TestCase):
         end_room = Room(0, 20, 10, 10)
         self.assertEqual(self.astar.choose_door(start_room, end_room),
                          ((5, 10), (5, 20)))
+
+    def test_finds_the_least_costly_path(self):
+        room1 = Room(10, 10, 10, 10)
+        room2 = Room(30, 10, 10, 10)
+        self.assertEqual(self.astar.get_astar_paths([(room1, room2)], [room1, room2]),
+                         [[(20, 15), (21, 15), (22, 15), (23, 15), (24, 15), (25, 15), (26, 15), (27, 15),
+                           (28, 15), (29, 15), (30, 15)]])
+
+    def test_chooses_the_same_path_when_going_back_and_forth(self):
+        room1 = Room(10, 10, 10, 10)
+        room2 = Room(30, 10, 10, 10)
+        expected_path = [(20, 15), (21, 15), (22, 15), (23, 15), (24, 15), (25, 15), (26, 15), (27, 15),
+                         (28, 15), (29, 15), (30, 15)]
+        path_backwards = expected_path[::-1]
+        self.assertEqual(self.astar.get_astar_paths([(room1, room2), (room2, room1)], [room1, room2]),
+                         [expected_path, path_backwards])
+
+    def test_chooses_the_same_path_no_matter_which_room_is_start_room(self):
+        room1 = Room(10, 10, 10, 10)
+        room2 = Room(30, 10, 10, 10)
+        path = self.astar.get_astar_paths([(room1, room2)], [room1, room2])
+        backwards_path = path[0][::-1]
+
+        another_astar = Astar()
+
+        self.assertEqual(backwards_path, another_astar.get_astar_paths(
+            [(room2, room1)], [room1, room2])[0])
+
+    def test_algorithm_doesnt_produce_diagonal_movement(self):
+        start_room = Room(0, 0, 10, 10)
+        end_room = Room(10, 10, 10, 10)
+        expected_path = [(0, 0), (1, 1), (2, 2), ..., (10, 10)]
+        self.assertNotEqual(self.astar.get_astar_paths([(start_room, end_room)], [start_room, end_room]),
+                            [expected_path])
+
+    def test_paths_dont_go_through_walls_in_maze(self):
+        self.astar.maze[25][15] = 100
+        room1 = Room(10, 10, 10, 10)
+        room2 = Room(30, 10, 10, 10)
+        expected_path = [(20, 15), (21, 15), (22, 15), (23, 15), (24, 15), (24, 14),
+                         (25, 14), (26, 14), (26, 15), (27, 15),
+                         (28, 15), (29, 15), (30, 15)]
+        self.assertEqual(self.astar.get_astar_paths([(room1, room2)], [room1, room2]),
+                         [expected_path])
